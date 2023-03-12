0a9088e59372fd8e9243a3c11cbb2e491750f23e
Sumedh Mohane <sumedhmohane@gmail.com>
2023-03-12 18:47:51 +0530

    segregating exact and partial interest matches

--- utils/ml_utils.py
+++ utils/ml_utils.py
@@ -39,6 +39,7 @@ def get_recommendations(user_pref, metadata_list, num_recommendations=15):
 
     # get the subset of product metadata based on user preferences
     subset_metadata = product_metadata.copy()
+    other_subset_metadata = subset_metadata
 
     # if ('interests' in user_pref) and (subset_metadata.empty == False) and (user_pref['interests'] != 'any') and ('any' not in subset_metadata['interests']):
     #     subset_metadata = subset_metadata[subset_metadata['interests'].apply(
@@ -46,8 +47,11 @@ def get_recommendations(user_pref, metadata_list, num_recommendations=15):
 
     if ('interests' in user_pref) & (subset_metadata.empty == False) and ('any' not in user_pref['interests']):
         subset_metadata = subset_metadata[subset_metadata['interests'].apply(
-            lambda x: 'any' in x or any(
+            lambda x:  any(
                 interest in x for interest in user_pref['interests'])
+        )]
+        other_subset_metadata = other_subset_metadata[other_subset_metadata['interests'].apply(
+            lambda x: 'any' in x
         )]
 
     if ('occasion' in user_pref) & (subset_metadata.empty == False) and ('any' not in user_pref['occasion']):
@@ -55,25 +59,38 @@ def get_recommendations(user_pref, metadata_list, num_recommendations=15):
             lambda x: 'any' in x or any(
                 occasion in x for occasion in user_pref['occasion'])
         )]
+        other_subset_metadata = other_subset_metadata[other_subset_metadata['occasions'].apply(
+            lambda x: 'any' in x or any(
+                occasion in x for occasion in user_pref['occasion'])
+        )]
 
     if ('relationship' in user_pref) and (subset_metadata.empty == False) and user_pref['relationship'].lower() != 'any':
         subset_metadata = subset_metadata[[user_pref['relationship'].lower() in [r.lower(
         ) for r in relationships] or 'any' in relationships for relationships in subset_metadata['relationships']]]
+        other_subset_metadata = other_subset_metadata[[user_pref['relationship'].lower() in [r.lower(
+        ) for r in relationships] or 'any' in relationships for relationships in other_subset_metadata['relationships']]]
 
     # get the row indices of the products in the subset
     product_indices = subset_metadata.index.tolist()
+    other_product_indices = other_subset_metadata.index.tolist()
 
     # get the cosine similarity scores for the products in the subset
     similarity_scores = [(i, cosine_sim[i][product_indices].mean())
                          for i in range(cosine_sim.shape[0])]
+    other_similarity_scores = [(i, cosine_sim[i][other_product_indices].mean())
+                               for i in range(cosine_sim.shape[0])]
 
     # sort the similarity scores in descending order
     similarity_scores = sorted(
         similarity_scores, key=lambda x: x[1], reverse=True)
 
+    other_similarity_scores = sorted(
+        other_similarity_scores, key=lambda x: x[1], reverse=True)
+
     # use the generated score along with similarity to rank the products
     ranked_scores = []
     for i in range(len(product_metadata)):
+        final_score = 0
         if i in product_indices:
             similarity_weight = 0.8
             product_weight = 0.2
@@ -86,15 +103,40 @@ def get_recommendations(user_pref, metadata_list, num_recommendations=15):
 
     ranked_scores = sorted(ranked_scores, key=lambda x: x[1], reverse=True)
 
+    # use the generated score along with similarity to rank the products
+    other_ranked_scores = []
+    for i in range(len(other_subset_metadata)):
+        final_score = 0
+        if i in product_indices:
+            similarity_weight = 0.8
+            product_weight = 0.2
+            final_score = similarity_weight * \
+                other_similarity_scores[i][1] + product_weight * \
+                product_metadata.loc[i, 'score']
+            other_ranked_scores.append((i, final_score))
+        else:
+            other_ranked_scores.append((i, 0))
+
+    other_ranked_scores = sorted(
+        other_ranked_scores, key=lambda x: x[1], reverse=True)
+
     # get the indices of the top n most similar products
     top_indices = [
         i[0] for i in ranked_scores[:num_recommendations] if not math.isnan(i[1])]
 
+    # get the indices of the top n most other similar products
+    other_top_indices = [
+        i[0] for i in other_ranked_scores[:num_recommendations] if not math.isnan(i[1])]
+
     # get the product IDs of the top n most similar products
     productIds = list(product_metadata.iloc[top_indices]['product_id'])
+    otherproductIds = list(
+        product_metadata.iloc[other_top_indices]['product_id'])
 
     # get the product metadata of the top n most similar products
     top_products_metadata = product_metadata.iloc[top_indices]
+    # get the product metadata of the top n most similar products
+    other_top_products_metadata = product_metadata.iloc[other_top_indices]
 
     # create a dictionary with the product IDs as keys and the product metadata as values
     top_products_dict = {}
@@ -109,6 +151,20 @@ def get_recommendations(user_pref, metadata_list, num_recommendations=15):
         }
         top_products_dict[product_id] = data
 
-    result_dict = {"productIds": productIds, "products": top_products_dict}
+    # create a dictionary with the product IDs as keys and the product metadata as values
+    other_top_products_dict = {}
+    for i in range(len(other_top_products_metadata)):
+        product_id = int(other_top_products_metadata.iloc[i]['product_id'])
+        product_data = other_top_products_metadata.iloc[i].to_dict()
+        data = {
+            'interests': product_data.get('interests', []),
+            'gender': product_data.get('gender', []),
+            'relationships': product_data.get('relationships', []),
+            'occasions': product_data.get('occasions', []),
+        }
+        other_top_products_dict[product_id] = data
+
+    result_dict = {"productIds": productIds, "products": top_products_dict,
+                   "otherProductIds": otherproductIds, "otherProducts": other_top_products_dict}
 
     return result_dict
